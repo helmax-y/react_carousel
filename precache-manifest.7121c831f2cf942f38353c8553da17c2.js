134e3c2f8eda552dea29b31bb7e8f1f9c2856305
helmax-y <max_rad@ukr.net>
2019-12-23 22:10:07 +0200

    Updates

---
+++ precache-manifest.7121c831f2cf942f38353c8553da17c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93dd2486aa1fc02550111a0f4c1a37fa",
+    "url": "/react_carousel/index.html"
+  },
+  {
+    "revision": "55886baf2094a9f6afd7",
+    "url": "/react_carousel/static/css/main.6ec257bc.chunk.css"
+  },
+  {
+    "revision": "769db312e4ebbc5e6277",
+    "url": "/react_carousel/static/js/2.29f9547f.chunk.js"
+  },
+  {
+    "revision": "55886baf2094a9f6afd7",
+    "url": "/react_carousel/static/js/main.1ee7f283.chunk.js"
+  },
+  {
+    "revision": "a62ff4406f9253df9292",
+    "url": "/react_carousel/static/js/runtime~main.30902e3a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_carousel/precache-manifest.50801b1752512efc4ec66a2e23e808f4.js"
+  "/react_carousel/precache-manifest.7121c831f2cf942f38353c8553da17c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_carousel/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
